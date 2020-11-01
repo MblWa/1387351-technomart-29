33abdca90879f10fa64c182fb411f54ea623d91f
MblWa <MblWa@users.noreply.github.com>
2020-11-02 01:15:22 +0300

    Добавлен второй слайдер для списка каталога

---
+++ js/catalog-slider.js
@@ -0,0 +1,29 @@
+var sliderButtonsDots = document.getElementsByClassName("slider-navigation-button-dot"); //коллекция радиокнопок
+var sliderNavButtonLeft = document.querySelector(".slider-navigation-button.left-nav-but");
+var sliderNavButtonRight = document.querySelector(".slider-navigation-button.right-nav-but");
+var sliderCatalogSlides = document.getElementsByClassName("slider-list-item"); //коллекция слайдов
+var currentSlide = 0; //инициализация индекса при открытии страницы
+
+sliderNavButtonLeft.addEventListener("click", function (evt) { //зададим логику левой кнопке при клике
+  evt.preventDefault(); //отменяем стандартное действие
+  console.log(currentSlide);
+  if (currentSlide - 1 >= 0) {
+    sliderCatalogSlides[currentSlide].classList.add("hidden-slide");
+    sliderButtonsDots[currentSlide].classList.remove("selected-slide");
+    currentSlide--;
+    sliderCatalogSlides[currentSlide].classList.remove("hidden-slide");
+    sliderButtonsDots[currentSlide].classList.add("selected-slide");
+  };
+});
+
+sliderNavButtonRight.addEventListener("click", function (evt) { //зададим логику правой кнопке при клике
+  evt.preventDefault(); //отменяем стандартное действие
+  console.log(currentSlide + 1);
+  if (currentSlide + 1 < sliderCatalogSlides.length) {
+    sliderCatalogSlides[currentSlide].classList.add("hidden-slide");
+    sliderButtonsDots[currentSlide].classList.remove("selected-slide");
+    currentSlide++;
+    sliderCatalogSlides[currentSlide].classList.remove("hidden-slide");
+    sliderButtonsDots[currentSlide].classList.add("selected-slide");
+  };
+});
